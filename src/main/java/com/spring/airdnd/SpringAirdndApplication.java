b42b4085c682940581d4fc59c06ee87a1fc2faca
changuikim <cu.kim.dev@gmail.com>
2024-06-21 11:16:29 +0900

    Airdnd Spring Project Initialized

---
+++ src/main/java/com/spring/airdnd/SpringAirdndApplication.java
@@ -0,0 +1,13 @@
+package com.spring.airdnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAirdndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAirdndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/airdnd/SpringAirdndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.airdnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAirdndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
