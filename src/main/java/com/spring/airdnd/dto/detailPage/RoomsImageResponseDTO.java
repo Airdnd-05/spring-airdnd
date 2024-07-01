e3bd6d21aa73645012329b9758e1ccb57b27976d
ptokos <jiseokchoij@gmail.com>
2024-07-01 11:19:56 +0900

    [Feat]: 숙소 이미지를 ID값으로 불러오는 기능 추가

--- src/main/java/com/spring/airdnd/dto/detailPage/RoomsImageResponseDTO.java
+++ src/main/java/com/spring/airdnd/dto/detailPage/RoomsImageResponseDTO.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class RoomsImageResponseDTO {
+    private Long roomId;
     private String mainImage;
     private String[] detailImage;
 
     public static RoomsImageResponseDTO from(Room room) {
         return new RoomsImageResponseDTO(
+                room.getRoomId(),
                 room.getRoomUrl1(),
                 new String[]{room.getRoomUrl2(), room.getRoomUrl3(), room.getRoomUrl4(), room.getRoomUrl5()}
         );
