1401e3415e574b7eb90fbd924e619e87c3f3d47f
seoeunhyo <seoeunhyo99@gmail.com>
2024-07-02 17:33:06 +0900

    [Fix] ContentController 경로 수정

---
+++ src/main/java/com/spring/airdnd/controller/ContentController.java
@@ -0,0 +1,31 @@
+package com.spring.airdnd.controller;
+
+
+import com.spring.airdnd.dto.ContentResponseDTO;
+import com.spring.airdnd.service.ContentService;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.descriptor.web.ContextService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/content")
+public class ContentController {
+
+    private final ContentService contentService;
+
+    @GetMapping("/{contentName}")
+    public ResponseEntity<List<ContentResponseDTO>> getBookingOptionContent(@PathVariable String contentName){
+
+        List<ContentResponseDTO> contentList = contentService.findByContentName(contentName);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(contentList);
+
+    }
+}
